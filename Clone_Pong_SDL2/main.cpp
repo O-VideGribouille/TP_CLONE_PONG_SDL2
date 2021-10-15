8c3cce0e9c2e9fe902489ce9667b661e4f6afdbe
O'Vide <56994268+O-VideGribouille@users.noreply.github.com>
2021-10-15 11:27:14 +0200

    Commit - integration surface + texture sprite

--- Clone_Pong_SDL2/main.cpp
+++ Clone_Pong_SDL2/main.cpp
@@ -20,7 +20,46 @@ int main(int argc, char** argv) {
     //Texture de background
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+    //Integration de Sprites non-dessinés
+    //IMG balle
+    SDL_Surface* surfaceBall;
+    SDL_Texture* textureBall;
+    surfaceBall = SDL_LoadBMP("./image/balle.bmp");
+    textureBall = SDL_CreateTextureFromSurface(renderer, surfaceBall);
+    SDL_FreeSurface(surfaceBall);
 
+    int scoring1 = 0;// servira pour le changement de score sans devoir créer d'autres image à la suite
+
+    string scrlink1 = "./image/" + to_string(scoring1) + ".bmp";// convertie l'integer en chaine de caractères
+    //IMG Score Joueur1
+    SDL_Surface* surfaceSCR1;
+    SDL_Texture* textureSCR1;
+    surfaceSCR1 = SDL_LoadBMP(const_cast<char*>(scrlink1.c_str())); //permet de l'intégrer à une adresse et lire l'image
+    textureSCR1 = SDL_CreateTextureFromSurface(renderer, surfaceSCR1);
+    SDL_FreeSurface(surfaceSCR1);
+
+    int scoring2 = 0;
+    string scrlink2 = "./image/" + to_string(scoring2) + ".bmp";
+    //IMG Score Joueur2
+    SDL_Surface* surfaceSCR2;
+    SDL_Texture* textureSCR2;
+    surfaceSCR2 = SDL_LoadBMP(const_cast<char*>(scrlink2.c_str()));
+    textureSCR2 = SDL_CreateTextureFromSurface(renderer, surfaceSCR2);
+    SDL_FreeSurface(surfaceSCR2);
+
+
+    //IMG Joueur1
+    SDL_Surface* surfaceJ1;
+    SDL_Texture* textureJ1;
+    surfaceJ1 = SDL_LoadBMP("./image/Joueur1.bmp");
+    textureJ1 = SDL_CreateTextureFromSurface(renderer, surfaceJ1);
+    SDL_FreeSurface(surfaceJ1);
+    //IMG Joueur2
+    SDL_Surface* surfaceJ2;
+    SDL_Texture* textureJ2;
+    surfaceJ2 = SDL_LoadBMP("./image/Joueur2.bmp");
+    textureJ2 = SDL_CreateTextureFromSurface(renderer, surfaceJ2);
+    SDL_FreeSurface(surfaceJ2);
 
 
 
