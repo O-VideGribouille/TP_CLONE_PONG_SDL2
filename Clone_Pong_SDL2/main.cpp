5d2c1b47c2134a223fbd71dbda11ca611a01329b
O'Vide <56994268+O-VideGribouille@users.noreply.github.com>
2021-10-15 14:18:41 +0200

    Commit - 6

--- Clone_Pong_SDL2/main.cpp
+++ Clone_Pong_SDL2/main.cpp
@@ -13,6 +13,8 @@
 
 using namespace std;
 
+
+
 int main(int argc, char** argv) {
 
     //Début et initialisation
@@ -28,13 +30,13 @@ int main(int argc, char** argv) {
     //IMG balle
     SDL_Surface* surfaceBall;
     SDL_Texture* textureBall;
-    surfaceBall = SDL_LoadBMP("./image/balle.bmp");
+    surfaceBall = SDL_LoadBMP("./Sprite/balle.bmp");
     textureBall = SDL_CreateTextureFromSurface(renderer, surfaceBall);
     SDL_FreeSurface(surfaceBall);
 
     int scoring1 = 0;// servira pour le changement de score sans devoir créer d'autres image à la suite
 
-    string scrlink1 = "./image/" + to_string(scoring1) + ".bmp";// convertie l'integer en chaine de caractères
+    string scrlink1 = "./Sprite/" + to_string(scoring1) + ".bmp";// convertie l'integer en chaine de caractères
     //IMG Score Joueur1
     SDL_Surface* surfaceSCR1;
     SDL_Texture* textureSCR1;
@@ -43,7 +45,7 @@ int main(int argc, char** argv) {
     SDL_FreeSurface(surfaceSCR1);
 
     int scoring2 = 0;
-    string scrlink2 = "./image/" + to_string(scoring2) + ".bmp";
+    string scrlink2 = "./Sprite/" + to_string(scoring2) + ".bmp";
     //IMG Score Joueur2
     SDL_Surface* surfaceSCR2;
     SDL_Texture* textureSCR2;
@@ -55,13 +57,13 @@ int main(int argc, char** argv) {
     //IMG Joueur1
     SDL_Surface* surfaceJ1;
     SDL_Texture* textureJ1;
-    surfaceJ1 = SDL_LoadBMP("./image/Joueur1.bmp");
+    surfaceJ1 = SDL_LoadBMP("./Sprite/Joueur1.bmp");
     textureJ1 = SDL_CreateTextureFromSurface(renderer, surfaceJ1);
     SDL_FreeSurface(surfaceJ1);
     //IMG Joueur2
     SDL_Surface* surfaceJ2;
     SDL_Texture* textureJ2;
-    surfaceJ2 = SDL_LoadBMP("./image/Joueur2.bmp");
+    surfaceJ2 = SDL_LoadBMP("./Sprite/Joueur2.bmp");
     textureJ2 = SDL_CreateTextureFromSurface(renderer, surfaceJ2);
     SDL_FreeSurface(surfaceJ2);
 
@@ -71,7 +73,7 @@ int main(int argc, char** argv) {
 
     //Initialisation emplacement et taille
 
-   
+
     //init Score Joueur1
     SDL_Rect Score1;
     Score1.x = 300;
@@ -87,76 +89,96 @@ int main(int argc, char** argv) {
 
 
     //Init raquette Joueur1
-    // CJoueur Raquette1(750, (600 / 2) - (100 / 2), 32, 100);
+    CJoueur Raquette1(750, (600 / 2) - (100 / 2), 32, 100);
     SDL_Rect playerPadle1;
-    playerPadle1.x = 750; //Raquette1.n_x;
-    playerPadle1.y = (600 / 2) - (100 / 2); //Raquette1.n_y; 
-    playerPadle1.w = 32; //Raquette1.n_w;
-    playerPadle1.h = 100; //Raquette1.n_h;
+    playerPadle1.x = Raquette1.getX();
+    playerPadle1.y = Raquette1.getY(); 
+    playerPadle1.w = Raquette1.getW();
+    playerPadle1.h = Raquette1.getH();
 
-    
+
 
     //Init raquette Joueur2
-    //CJoueur Raquette2(20, (600 / 2) - (100 / 2), 32, 100 );
-    SDL_Rect playerPadle2; 
-    playerPadle2.x = 20; // Raquette2.n_x;
-    playerPadle2.y = (600 / 2) - (100 / 2); // Raquette2.n_y;
-    playerPadle2.w = 32; // Raquette2.n_w;
-    playerPadle2.h = 100; // Raquette2.n_h;
+    CJoueur Raquette2(20, (600 / 2) - (100 / 2), 32, 100 );
+    SDL_Rect playerPadle2;
+    playerPadle2.x = Raquette2.getX();
+    playerPadle2.y = Raquette2.getY();
+    playerPadle2.w = Raquette2.getW();
+    playerPadle2.h = Raquette2.getH();
 
     //Init Balle
-    CBalle Ball(((800 / 2) - (20 / 2)), ((600 / 2) - (20 / 2)), 32, 32);
+    CBalle cBalle(((800 / 2) - (20 / 2)), ((600 / 2) - (20 / 2)), 32, 32);
 
-    SDL_Rect ball;
-    ball.x = Ball.&getX;
-    ball.y = Ball.&getY;
-    ball.w = Ball.&getW;
-    ball.h = Ball.&getH;
-   
-
-
-    //Sert au déplacement de la balle
-    float bX = ball.x;
-    float bY = ball.y;
+       SDL_Rect ball;
+       ball.x = cBalle.getX();
+       ball.y = cBalle.getY();
+       ball.w = cBalle.getW();
+       ball.h = cBalle.getH();
 
 
 
+       //Sert au déplacement de la balle
+       float bX = ball.x;
+       float bY = ball.y;
 
-    // Jeu
+
+
+
+       // Jeu
     while (!quit) {
 
-        //Event
-        while (SDL_PollEvent(&e) > 0) {
-            if (e.type == SDL_QUIT) {
-                quit = true;
-            }
+         //Event
+         while (SDL_PollEvent(&e) > 0) {
+             if (e.type == SDL_QUIT) {
+                 quit = true;
+             }
 
-            else if (e.type == SDL_KEYDOWN) {
-            
-            
-            }
+           /*  else if (e.type == SDL_KEYDOWN) {
 
+
+
+             }*/
+
+
+
+
+
+                     // Renderer et dessin
+              SDL_SetRenderDrawColor(renderer, 0, 27, 27, 27);// Color le fond en noir
+             SDL_RenderClear(renderer); // Efface l'écran et les couleur disposées;
+
+
+               //écriture du score
+              SDL_RenderCopy(renderer, textureSCR1, NULL, &Score1);
+               SDL_RenderCopy(renderer, textureSCR2, NULL, &Score2);
+
+              //dessiner les raquettes de joueurs
+              SDL_RenderCopy(renderer, textureJ1, NULL, &playerPadle1);
+              SDL_RenderCopy(renderer, textureJ2, NULL, &playerPadle2);
 
         
+             //dessiner la balle
+              SDL_RenderCopy(renderer, textureBall, NULL, &ball);
 
 
 
+          }
 
             // Affiche le nouveau contenu
             SDL_RenderPresent(renderer);
-    }
+     }
 
 
 
 
 
 
-    //Si quit = true
+        //Si quit = true
 
-    //Fin
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+        //Fin
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
 
 	return 0;
 }
