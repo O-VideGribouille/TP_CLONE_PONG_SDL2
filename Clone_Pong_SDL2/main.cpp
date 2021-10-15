4f4988aeb6496663af58d184f338cf983a76a78e
O'Vide <56994268+O-VideGribouille@users.noreply.github.com>
2021-10-15 11:15:06 +0200

    Commit - 4

--- Clone_Pong_SDL2/main.cpp
+++ Clone_Pong_SDL2/main.cpp
@@ -1,6 +1,34 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <string.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <time.h>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    //Début et initialisation
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    //Création d'une fenêtre
+    SDL_Window* window = SDL_CreateWindow("1. Pong C++ SDL2", 50, 50, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Event e;
+    //Texture de background
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
 
-int main() {
+
+
+
+
+    //Fin
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
 	return 0;
 }
